6714f97f9827ac9db13d1c3e399127ca6fd06172
Jennifer Nerad <jnerad@gmail.com>
2015-03-05 10:42:01 -0800

    added random image

--- app/script.js
+++ app/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x','y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&'];
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x','y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var randomString = '';
-    function randomStringFromArray(array) {
-      var tempArray = [];
-      var randomNum = Math.floor((Math.random() * 25) + 1);
-      for (var i = 0; i < randomNum; i++) {
-        var randomNumber = Math.floor(Math.random() * array.length);
-        tempArray[i] = array[randomNumber];
-      }
-      randomString = tempArray.join('');
-      return randomString;
+  
+  function randomStringFromArray(array) {
+    var tempArray = [];
+    var randomNum = Math.floor((Math.random() * 25) + 1);
+    for (var i = 0; i < randomNum; i++) {
+      var randomNumber = Math.floor(Math.random() * array.length);
+      tempArray[i] = array[randomNumber];
     }
+    randomString = tempArray.join('');
+    return randomString;
+  }
+
+
 
   $('#myButton').on('click', function(){
     randomStringFromArray(alphabet);
@@ -19,20 +22,26 @@ $(document).ready(function() {
   // $('#jokes').on('click', function(){
   //   $('#ajax-text').append('Hello world');
   // });
+  $('#photos').on('click', function() {
+    var url = $(this).attr('id');
+    var photosHTML;
+    $.get(url, function (response) {
+      photosHTML = '<img src=\"' + response.src + '\" alt=\"' + response.alt + '\" >';
+    $('#photos').html(photosHTML);
+    });
+   }); 
 
   $('#jokes').on('click', function() {
     var url = $(this).attr('id');
-
-  $.get(url, function (response) {
-  var resText;
-
-  if(typeof response === 'object'){
-    resText = response.setup + ': ' + response.punchline;
-  } else {
-    resText = response;
-  }
-  $('#ajax-text').text(resText);
-});
-});
+    $.get(url, function (response) {
+    var resText;
+      if(typeof response === 'object'){
+        resText = response.setup + ': ' + response.punchline;
+      } else {
+        resText = response;
+      }
+      $('#ajax-text').text(resText);
+    });
+  });
 
 });
--- server.js
+++ server.js
@@ -4,29 +4,48 @@ var port = process.env.PORT || 3000;
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x','y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&'];
 var randomString = '';
 var jokes = [
-{setup: 'What do you call a masturbating cow?',
-punchline: 'Beef stroking off!'},
+{setup: 'Why don\'t lions play cards in the jungle?',
+punchline: 'Too many cheetahs.'},
 {setup: 'What do you call a masseuse who hates women?',
 punchline: 'A (massage)ynist!'},
-{setup: 'What did the zero say to the eight?',
-punchline: 'nice belt'}
+{setup: 'What did the 0 say to the 8?',
+punchline: 'Nice belt.'}
+];
+var photos = [
+{alt: 'grey toned photo of birds',
+  src: 'images/pigeonmurmuration1.png'},
+{alt: 'blue toned photo of birds',
+  src: 'images/pigeonmurmuration2.png'},
+{alt: 'yellow toned photo of birds',
+src: 'images/pigeonmurmuration3.png'},
+{alt: 'red toned photo of birds',
+src: 'images/pigeonmurmuration4.png'}
 ];
 // use /app as home folder
 app.use(express.static(__dirname + '/app/'));
 
 //make index.html home page
-app.get('/', function(req, res){
+app.get('/', function(req, res){ //endpoint #1
   res.sendFile('index.html');
 });
 
+//add the photo on page load
+
+
+//serve random photo when it's clicked on
+app.get('/photos', function(req, res) {
+  var randomIndex = Math.floor(Math.random() * photos.length);
+  res.json(photos[randomIndex]);
+});
+
 // serve random joke from json object
-app.get('/jokes', function(req, res){
+app.get('/jokes', function(req, res){ //endpoint #2
   var randomIndex = Math.floor(Math.random() * jokes.length);
   res.json(jokes[randomIndex]);
 });
 
 // use string generator at /random-strings page
-app.get('/random-strings', function(req, res){
+app.get('/random-strings', function(req, res){ //endpoint #3
   var tempArray = [];
       var randomNum = Math.floor((Math.random() * 25) + 1);
       for (var i = 0; i < randomNum; i++) {
